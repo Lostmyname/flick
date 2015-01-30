aa6fd3d02a929b2664566c0d3e20725823e2ca05
Callum Macrae <callum@macr.ae>
2015-01-30 16:18:27 +0000

    Added basics

--- src/js/flick.js
+++ src/js/flick.js
@@ -1,14 +1,41 @@
-window.Component = module.exports = (function () {
-  'use strict';
+'use strict';
 
-  /**
-   * Your documentation here. Not optional!
-   */
-  function Component() {
-  }
+var $ = require('jquery');
 
-  Component.CONSTANT = 7;
-//  Component._dependency = require('./blablabla');
+$.flick = {};
 
-  return Component;
-})();
+$.flick.mouseInput = function flickMouseInput() {
+  var $this = $(this);
+  var $inputHandler = $({});
+
+  var offset = $this.offset().left;
+  var width = $this.find('img').eq(0).width();
+
+  $this.on('mousemove', function (e) {
+    var left = e.pageX - offset;
+    $inputHandler.triggerHandler('change', left / width);
+  });
+
+  return $inputHandler;
+};
+
+$.flick.defaults = {
+  input: $.flick.mouseInput
+};
+
+$.fn.flick = function flick(options) {
+  options = $.extend({}, $.flick.defaults, options);
+
+  var $inputHandler = options.input.call(this, options);
+  var $pages = $(this).find('img');
+
+  $inputHandler.on('change', function (e, percent) {
+    var pageNumber = Math.floor(($pages.length + 1) * percent);
+
+    $pages.css('z-index', 1)
+      .eq(pageNumber).css('z-index', 2);
+  });
+};
+
+// Automatically flick from data-key
+$('.js-flick').flick();
