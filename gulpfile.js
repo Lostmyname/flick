4c7b11ad168541b67aa219645887e482f850c745
Tom Cartwright <tecartwright@gmail.com>
2015-03-19 15:23:57 +0000

    Tidy up consoles

--- gulpfile.js
+++ gulpfile.js
@@ -9,11 +9,6 @@ var buildPath = './demo/build/';
 
 gulp.task('auto-reload', loadLmnTask('auto-reload'));
 
-// gulp.task('html', loadLmnTask('html', {
-//   langBase: 'component.flick',
-//   imagePath: '../../src/imgs/'
-// }));
-
 gulp.task('js', ['js-quality'], loadLmnTask('browserify', {
   src: './src/js/flick.js',
   dest: path.join(buildPath, 'js/bundle.js')
